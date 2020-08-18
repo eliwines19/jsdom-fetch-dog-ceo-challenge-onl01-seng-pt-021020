62160910120730e75cce80ea30d1d06bbb15d615
Elijah wines <elijahwines84@yahoo.com>
2020-08-18 12:21:57 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
 console.log('%c HI', 'color: firebrick')
-document.addEventListener("DOMContenLoaded", function() {
 
-  let dogBreedsUl = document.getElementById('dog-breeds');
-
-  function renderImages(images) {
-    const dogImageContainer = document.getElementById('dog-image-container');
-    images.forEach(image => {
-      const img = document.createElement('img');
-      img.src = image;
-      dogImageContainer.appendChild(img);
-    })
-  };
-
-  function fetchDogImages() {
-    return fetch('https://dog.ceo/api/breeds/image/random/4')
-    .then(resp => resp.json())
-    .then(json => renderImages(json))
-  };
-
-  fetchDogImages();
-
-});
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
