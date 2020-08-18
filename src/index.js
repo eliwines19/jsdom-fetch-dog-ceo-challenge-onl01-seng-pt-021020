26018e08389fc643d433066b0da4e147798a3f65
Elijah wines <elijahwines84@yahoo.com>
2020-08-18 12:23:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ console.log('%c HI', 'color: firebrick')
 
 const breedUrl = "https://dog.ceo/api/breeds/list/all";
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+function fetchBreeds() {
+  fetch(breedUrl)
+  .then(resp => resp.json())
+  .then(json => postBreed(json))
+};
