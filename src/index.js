0f5f03eccd26ad763770b9a116b55c6d67d22c8d
Elijah wines <elijahwines84@yahoo.com>
2020-08-18 12:28:18 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,3 +8,25 @@ function fetchBreeds() {
   .then(resp => resp.json())
   .then(json => postBreed(json))
 };
+
+function postBreed(json) {
+  let breedList = document.getElementById('dog-breeds');
+  for (let key in json.messages) {
+    let breed = document.createElement('li');
+    if (json message[key].length > 0) {
+      json.message[key].forEach(e => {
+        breed.innerHTML = `${e} ${key}`;
+        breed.className = "breed";
+        breedList.append(breed);
+      })
+    }
+    else {
+      breed.innerHTML = `${key}`;
+      breed.className = "breed";
+      breedList.append(breed);
+    }
+  }
+  breedList.addEventListener("click", e => {
+    e.target.style.color = 'blue';
+  })
+};
