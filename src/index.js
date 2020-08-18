d4dfd53597957358e1c9317541e46597dbcaa113
Elijah wines <elijahwines84@yahoo.com>
2020-08-18 12:09:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 console.log('%c HI', 'color: firebrick')
 document.addEventListener("DOMContenLoaded", function() {
+
   let dogImageContainer = document.getElementById('dog-image-container');
   let dogBreedsUl = document.getElementById('dog-breeds');
+  
 });
