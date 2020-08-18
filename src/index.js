3e023014e313237fa52fa045bef7b5eca6384a4a
Elijah wines <elijahwines84@yahoo.com>
2020-08-18 12:10:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ document.addEventListener("DOMContenLoaded", function() {
 
   let dogImageContainer = document.getElementById('dog-image-container');
   let dogBreedsUl = document.getElementById('dog-breeds');
-  
+
+  function fetchDogImages() {
+    return fetch('https://dog.ceo/api/breeds/image/random/4') 
+  };
+
 });
